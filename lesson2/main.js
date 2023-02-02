17f690a409d84b875375eea0678feb9d92641568
Limbovski1 <ivashchuk.dmyro@gmail.com>
2023-02-03 01:38:02 +0200

    1 tasl complete?

---
+++ lesson2/main.js
@@ -0,0 +1,103 @@
+arr = [true, false, 1, 999, -123, 'oleg', 15, 3.14, -2.8, 'okten'];
+console.log(arr);
+//
+//
+book1 = {
+    title: 'Harry Poter',
+    pageCount: 218,
+    genre: 'Fantasy',
+}
+console.log(book1);
+book2 = {
+    title: 'Berserk',
+    pageCount: 320,
+    genre: 'Horror',
+}
+console.log(book2);
+book3 = {
+    title: 'It ends with us',
+    pageCount: 275,
+    genre: 'Romance',
+}
+console.log(book3);
+//
+//
+book4 = {
+    title: 'Harry Poter',
+    pageCount: 218,
+    genre: 'Fantasy',
+    authors: [name = 'Joanne', age = 57]
+}
+console.log(book4);
+book5 = {
+    title: 'Harry Poter',
+    pageCount: 218,
+    genre: 'Fantasy',
+    authors: [name = 'Jacky', age = 32]
+}
+console.log(book5);
+book6 = {
+    title: 'Harry Poter',
+    pageCount: 218,
+    genre: 'Fantasy',
+    authors: [name = 'Rohas', age = 51]
+}
+console.log(book6);
+//
+//
+users = [
+    user1 = {
+    name: 'Olya',
+    username: 'Olga12',
+    password: 'GoweF182hH',
+    },
+    user2 = {
+    name: 'Sasha',
+    username: 'Rustam18',
+    password: 'GSADgasd34',
+    },
+    user3 = {
+    name: 'Dima',
+    username: 'Deymon1',
+    password: 'GoweF182hH',
+    },
+    user4 = {
+    name: 'Ivan',
+    username: 'VanyaTankist',
+    password: '1295JgRNnF',
+    },
+    user5 = {
+    name: 'Anna',
+    username: 'AnnaSophia',
+    password: '19g47FJRfhsU',
+    },
+    user6 = {
+    name: 'Yakiv',
+    username: 'Trx',
+    password: 'GjasdHR17',
+    },
+    user7 = {
+    name: 'Tolik',
+    username: 'AnatoliyGlek',
+    password: '19brJB7DN4nfs',
+    },
+    user8 = {
+    name: 'Dima',
+    username: 'DmitriyNg',
+    password: '20ban4jgJGNTs92',
+    },
+    user9 = {
+    name: 'Alex',
+    username: 'Alexandr1997',
+    password: 'KASusnrj7174',
+    },
+    user10 = {
+    name: 'Alina',
+    username: 'Alinina',
+    password: 'kNVh23fhsU',
+    },
+];
+console.log(users.user1.password);
+
+
+
